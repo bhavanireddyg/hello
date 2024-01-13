568462352301fed01cce9e617a06b99fa322915d
bhavanireddyg <bhavani.reddy2021@vitstudent.ac.in>
2024-01-13 16:58:06 +0530

    added bhavani program

---
+++ bhavani.java
@@ -0,0 +1,11 @@
+public class bhavani {
+    public static void main(String[] args) {
+        int n = 10; // You can change this value to print odd numbers up to a different limit
+
+        System.out.println("Odd numbers up to " + n + ":");
+
+        for (int i = 1; i <= n; i += 2) {
+            System.out.println(i);
+        }
+    }
+}
--- bhavanireddy.py
+++
@@ -1,5 +0,0 @@
-n = 10  
-print(f"Odd numbers up to {n}:")
-
-for i in range(1, n+1, 2):
-    print(i)
